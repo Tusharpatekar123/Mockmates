bf625c74e831e861ef0c8ca6f41525ea25e14240
shahid0609 <shahidpathan061@gmail.com>
2024-11-28 15:34:09 +0530

    add sample2

---
+++ src/sample1/Sample2.java
@@ -0,0 +1,31 @@
+package sample1;
+
+public class Sample2 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("hellow");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
